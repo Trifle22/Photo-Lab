b70d17eb246ad20d735e485870e6dd96f337c11d
Trifle22 <xromchenkov99@gmail.com>
2020-11-30 23:44:26 +0300

    first

---
+++ src/js/main.js
@@ -0,0 +1,56 @@
+
+let slideIndex = 1;
+const burgerButton = document.querySelector('.menu-btn');
+const burgerMenu = document.querySelector('.burger-menu');
+console.log(burgerMenu);
+
+showSlides(slideIndex);
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("swiper-slide");
+  let paginationButtons = document.querySelectorAll('.pagination--button');
+  console.log(paginationButtons);
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  paginationButtons.forEach(item => {
+    item.addEventListener('click', buttons)
+  });
+  
+  function buttons() {
+    paginationButtons.forEach(item => {
+      item.classList.remove('active');
+    })
+    this.classList.add('active');
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+
+}
+
+burgerButton.addEventListener('click', function() {
+  burgerButton.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+})
+
+
+
+
+
+
